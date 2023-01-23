472eb694872607130beafb0884d4f8204267b01e
Alexandra July <catmon0201@gmail.com>
2023-01-23 15:18:29 +0200

    first commit

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 let numbers = prompt('Введіть, будь ласка, 3 цифри через кому.','');
-let arr_Numbers = numbers.split(',');
-console.log(arr_Numbers);
-let result = arr_Numbers.reduce((sum, current) => sum + current); 
+let arrNumbers = numbers.split(',');
+console.log(arrNumbers);
+let result =  arrNumbers.reduce((sum, current) => sum + Number(current), 0); 
 console.log(result);
 let min;
-if(arr_Numbers[0] < arr_Numbers[1] && arr_Numbers[0] < arr_Numbers[2]) {
-    min = arr_Numbers[0]; 
-} else if(arr_Numbers[1] < arr_Numbers[0] && arr_Numbers[1] < arr_Numbers[2]) {
-    min = arr_Numbers[1];
-} else if(arr_Numbers[2] < arr_Numbers[0] && arr_Numbers[2] < arr_Numbers[1]) {
-    min = arr_Numbers[2];
+if(arrNumbers[0] < arrNumbers[1] && arrNumbers[0] < arrNumbers[2]) {
+    min = arrNumbers[0]; 
+} else if(arrNumbers[1] < arrNumbers[0] && arrNumbers[1] < arrNumbers[2]) {
+    min = arrNumbers[1];
+} else if(arrNumbers[2] < arrNumbers[0] && arrNumbers[2] < arrNumbers[1]) {
+    min = arrNumbers[2];
 }
 console.log(min);
 let max;
-if(arr_Numbers[0] > arr_Numbers[1] && arr_Numbers[0] > arr_Numbers[2]) {
-    max = arr_Numbers[0]; 
-} else if(arr_Numbers[1] > arr_Numbers[0] && arr_Numbers[1] > arr_Numbers[2]) {
-    max = arr_Numbers[1];
-} else if(arr_Numbers[2] > arr_Numbers[0] && arr_Numbers[2] > arr_Numbers[1]) {
-    max = arr_Numbers[2];
+if(arrNumbers[0] > arrNumbers[1] && arrNumbers[0] > arrNumbers[2]) {
+    max = arrNumbers[0]; 
+} else if(arrNumbers[1] > arrNumbers[0] && arrNumbers[1] > arrNumbers[2]) {
+    max = arrNumbers[1];
+} else if(arrNumbers[2] > arrNumbers[0] && arrNumbers[2] > arrNumbers[1]) {
+    max = arrNumbers[2];
 }
 console.log(max);
-alert('Перший номер це ' + arr_Numbers[0] + ',' + 'останній номер це ' + arr_Numbers[2] + `\n` + arr_Numbers[0] + '+' + arr_Numbers[1] + '+' + arr_Numbers[2] + ' = ' + result + `\n` +'Найменше число це ' + min + ',' + ' найбільше число це ' + max);
+alert('Перший номер це ' + arrNumbers[0] + ',' + 'останній номер це ' + arrNumbers[2] + `\n` + arrNumbers[0] + '+' + arrNumbers[1] + '+' + arrNumbers[2] + ' = ' + result + `\n` +'Найменше число це ' + min + ',' + ' найбільше число це ' + max);
 let number = prompt('Введіть, будь ласка одне число з тих, що ви вводили раніше.','');
 console.log(number);
-let en_num;
-if(number === arr_Numbers[0]) {
-    en_num = 'Перше число';
-} else if(number === arr_Numbers[1]) {
-    en_num = 'Друге число';
-} else if(number === arr_Numbers[2]) {
-    en_num = 'Третє число';
+let enNum;
+if(number === arrNumbers[0]) {
+    enNum = 'Перше число';
+} else if(number === arrNumbers[1]) {
+    enNum = 'Друге число';
+} else if(number === arrNumbers[2]) {
+    enNum = 'Третє число';
 } else {
-    en_num = 'Пробачте, але ви не вводили це число.';
+    enNum = 'Пробачте, але ви не вводили це число.';
 }
-console.log(en_num);
-alert(en_num);
+console.log(enNum);
+alert(enNum);
